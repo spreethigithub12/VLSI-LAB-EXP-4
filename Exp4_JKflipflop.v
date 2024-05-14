59384a4f2a7bc45668a216de2d5523c68a15d86f
spreethigithub12 <169068043+spreethigithub12@users.noreply.github.com>
2024-05-14 14:12:45 +0530

    Update Exp4_JKflipflop.v

--- Exp4_JKflipflop.v
+++ Exp4_JKflipflop.v
@@ -1,8 +1,19 @@
-6(ii): JK FLIP FLOP 
-module jk( j,k,clk,q); 
-input j,k,clk; 
-output reg q=0; 
-
-
-
+module jkff(clk,rst,j,k,q);
+input clk,rst,j,k;
+output reg q;
+always @(posedge clk)
+begin
+if(rst)
+ q<=1'b0;
+else
+ begin
+ case({j,k})
+ 2'b00:q<=q;
+ 2'b01:q<=1'b0;
+ 2'b10:q<=1'b1;
+ 2'b11:q<=~q;
+ default:q<=1'bx;
+ endcase
+ end
+end 
 endmodule
