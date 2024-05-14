15fc0d0c70bb84e219e702691a153f5b9e89cf01
spreethigithub12 <169068043+spreethigithub12@users.noreply.github.com>
2024-05-14 14:15:47 +0530

    Update Exp4_SRflipflop.v

--- Exp4_SRflipflop.v
+++ Exp4_SRflipflop.v
@@ -1,13 +1,19 @@
-module sr( s,r,clk,q);
- input s,r,clk; 
-output reg q=0; 
-
-
-
-
+module srff(clk,rst,s,r,q);
+input clk,rst,s,r;
+output reg q;
+always @(posedge clk)
+begin
+if(rst)
+ q<=1'b0;
+else
+ begin
+ case({s,r})
+ 2'b00:q<=q;
+ 2'b01:q<=1'b0;
+ 2'b10:q<=1'b1;
+ 2'b11:q<=1'bx;
+ default:q<=1'bx;
+ endcase
+ end
+end 
 endmodule
-
-
-
-
-
