ec846ef32d45f036c119318b8c05eca18a0a4dc0
spreethigithub12 <169068043+spreethigithub12@users.noreply.github.com>
2024-05-14 14:11:35 +0530

    Update Exp4_Dflipflop.v

--- Exp4_Dflipflop.v
+++ Exp4_Dflipflop.v
@@ -1,8 +1,11 @@
-6(iii): D FLIP FLOP 
-module dff( clk,d,q); 
-input clk,d; 
-output reg q; 
-
-
-
+module dff(d,clk,rst,q);
+input d,clk,rst;
+output reg q;
+always @(posedge clk)
+begin 
+if(rst)
+ q <=1'b0;
+else
+ q <= d;
+end
 endmodule
