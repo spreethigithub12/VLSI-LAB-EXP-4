c71fd911a634d40114344620084e70c6755882e4
spreethigithub12 <169068043+spreethigithub12@users.noreply.github.com>
2024-05-14 14:13:35 +0530

    Update Exp4_Mod10counter.v

--- Exp4_Mod10counter.v
+++ Exp4_Mod10counter.v
@@ -1,12 +1,12 @@
-//MOD -10 COUNTER 
-module mod10(rst,clk,out); 
-input rst,clk; 
-output reg[3:0]out=4'b0000;
- always@ (posedge clk) 
-begin 
-if(rst==1 | out==4'b1001) 
-out=4'b0000; 
-else 
-out=out+4'b0001;
- end 
+module mod10counter(clk,rst,count);
+input clk,rst;
+output[3:0]count;
+reg[3:0]count;
+always@(posedge clk)
+begin
+if(rst|count==4'b1001)
+count<=4'b0;
+else
+count<=count+1;
+end
 endmodule
